a95d8e861017655cd0b52b653ee42d2084dd132c
Dilip Patel <dilip_patel@discovery.com>
2023-07-03 18:02:09 +0530

    reading through firebase realtime database

--- app/src/main/java/com/example/firebaserealtimedatabasepoc/ReadActivity.kt
+++ app/src/main/java/com/example/firebaserealtimedatabasepoc/ReadActivity.kt
@@ -35,14 +35,54 @@ class ReadActivity : AppCompatActivity() {
             val myRef = database.getReference("message")
             myRef.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    // This method is called once with the initial value and again
-                    // whenever data at this location is updated.
-                    val value = dataSnapshot.getValue<HashMap<String, Item>>()
-                    val textToShow = (value?.get("recipe_detail_page") as ArrayList<RecipeDetailPage>)[0]
-                    tvReadDb.text = "textToShow"
-                    Toast.makeText(this@ReadActivity, value.toString(), Toast.LENGTH_SHORT).show()
-                    val dfd = emptySet<String>()
-                    val fdf = setOf<String>()
+                    // Check if the dataSnapshot exists
+                    if (dataSnapshot.exists()) {
+                        // Read the shop_landing_page array
+                        val shopLandingPageList = dataSnapshot.child("shop_landing_page").children
+                        for (landingPageSnapshot in shopLandingPageList) {
+                            // Read the section
+                            val section = landingPageSnapshot.child("section").value as String
+                            println("Section: $section")
+
+                            // Read the items array
+                            val itemsList = landingPageSnapshot.child("items").children
+                            for (itemSnapshot in itemsList) {
+                                // Read the item details
+                                val title = itemSnapshot.child("title").value as String
+                                val url = itemSnapshot.child("url").value as String
+                                val imageUrl = itemSnapshot.child("imageUrl").value as String
+                                val category = itemSnapshot.child("category").value as String
+
+                                println("Title: $title")
+                                println("URL: $url")
+                                println("Image URL: $imageUrl")
+                                println("Category: $category")
+                            }
+                        }
+
+                        // Read the recipe_detail_page array
+                        val recipeDetailPageList = dataSnapshot.child("recipe_detail_page").children
+                        for (detailPageSnapshot in recipeDetailPageList) {
+                            // Read the section
+                            val section = detailPageSnapshot.child("section").value as String
+                            println("Section: $section")
+
+                            // Read the items array
+                            val itemsList = detailPageSnapshot.child("items").children
+                            for (itemSnapshot in itemsList) {
+                                // Read the item details
+                                val title = itemSnapshot.child("title").value as String
+                                val url = itemSnapshot.child("url").value as String
+                                val imageUrl = itemSnapshot.child("imageUrl").value as String
+                                val category = itemSnapshot.child("category").value as String
+
+                                println("Title: $title")
+                                println("URL: $url")
+                                println("Image URL: $imageUrl")
+                                println("Category: $category")
+                            }
+                        }
+                    }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
